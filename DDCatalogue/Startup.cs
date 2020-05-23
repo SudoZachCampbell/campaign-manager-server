0bbbff2171084b1da5ce8813c65f6ebce709566d
40132243 <zcam1995@gmail.com>
2020-05-23 17:47:59 +0100

    Monster Controller Returning Table

--- DDCatalogue/Startup.cs
+++ DDCatalogue/Startup.cs
@@ -1,7 +1,8 @@
 using Amazon.S3;
+using DDCatalogue.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,8 @@ namespace DDCatalogue
 
             services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
             services.AddAWSService<IAmazonS3>();
+
+            services.AddScoped<DDContext>();
 
             services.AddCors(options =>
             {
