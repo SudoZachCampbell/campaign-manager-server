4f2890d209b01ee04f1fb71b2a958bd043a34c36
40132243 <zcam1995@gmail.com>
2020-05-27 13:00:17 +0100

    Made Proficiency Class

--- DDCatalogue/Model/Creatures/Creature.cs
+++ DDCatalogue/Model/Creatures/Creature.cs
@@ -1,4 +1,5 @@
 ﻿using DDCatalogue.Model.Locations;
+using System.Collections.Generic;
 
 namespace DDCatalogue.Model.Creatures
 {
@@ -12,30 +13,7 @@ namespace DDCatalogue.Model.Creatures
         public int? Intelligence { get; set; }
         public int? Wisdom { get; set; }
         public int? Charisma { get; set; }
-        public bool? Acrobatics { get; set; }
-        public bool? AnimalHandling { get; set; }
-        public bool? Arcana { get; set; }
-        public bool? Athletics { get; set; }
-        public bool? Deception { get; set; }
-        public bool? History { get; set; }
-        public bool? Insight { get; set; }
-        public bool? Intimidation { get; set; }
-        public bool? Investigations { get; set; }
-        public bool? Medicine { get; set; }
-        public bool? Nature { get; set; }
-        public bool? Perception { get; set; }
-        public bool? Performance { get; set; }
-        public bool? Persuasion { get; set; }
-        public bool? Religion { get; set; }
-        public bool? SleightOfHand { get; set; }
-        public bool? Stealth { get; set; }
-        public bool? Survival { get; set; }
-        public bool? SavStr { get; set; }
-        public bool? SavDex { get; set; }
-        public bool? SavCon { get; set; }
-        public bool? SavInt { get; set; }
-        public bool? SavWis { get; set; }
-        public bool? SavCha { get; set; }
+        public List<Proficiency> Proficiencies { get; set; }
         public int ArmorClass { get; set; }
         public int HitPoints { get; set; }
         public string HitDice { get; set; }
@@ -45,6 +23,12 @@ namespace DDCatalogue.Model.Creatures
         public Alignment Alignment { get; set; }
         public string[] Reactions { get; set; }
         public string Picture { get; set; }
+    }
+
+    public class Proficiency
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
     }
 
     public enum Alignment
