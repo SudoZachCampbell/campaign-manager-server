2632865cca38671b97de92f8636b55ccd9f2489b
Zach Campbell <zcam1995@gmail.com>
2020-06-08 19:43:02 +0100

    Started adding default values to Creature

--- DDCatalogue/Model/Creatures/Creature.cs
+++ DDCatalogue/Model/Creatures/Creature.cs
@@ -10,15 +10,15 @@ namespace DDCatalogue.Model.Creatures
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int? Strength { get; set; }
-        public int? Dexterity { get; set; }
-        public int? Constitution { get; set; }
-        public int? Intelligence { get; set; }
-        public int? Wisdom { get; set; }
-        public int? Charisma { get; set; }
-        public JArray Proficiencies { get; set; }
-        public int ArmorClass { get; set; }
-        public int HitPoints { get; set; }
+        public int Strength { get; set; } = 0;
+        public int Dexterity { get; set; } = 0;
+        public int Constitution { get; set; } = 0;
+        public int Intelligence { get; set; } = 0;
+        public int Wisdom { get; set; } = 0;
+        public int Charisma { get; set; } = 0;
+        public JArray Proficiencies { get; set; } = new JArray();
+        public int ArmorClass { get; set; } = 0;
+        public int HitPoints { get; set; } = 0;
         public string HitDice { get; set; }
         public string Size { get; set; }
         public JArray Speed { get; set; }
