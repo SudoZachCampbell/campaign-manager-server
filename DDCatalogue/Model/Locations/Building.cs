553a6edd11412aedc10f228273da1a939ee65f21
Zach Campbell <zcam1995@gmail.com>
2020-06-12 18:15:29 +0100

    Added Location Coords

--- DDCatalogue/Model/Locations/Building.cs
+++ DDCatalogue/Model/Locations/Building.cs
@@ -1,5 +1,6 @@
 ﻿using DDCatalogue.Model.Creatures;
 using DDCatalogue.Model.Joins;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 namespace DDCatalogue.Model.Locations
@@ -10,6 +11,7 @@ namespace DDCatalogue.Model.Locations
         public string Name { get; set; }
         public int? LocaleId { get; set; }
         public Locale Locale { get; set; }
+        public string[] LocationCoords { get; set; }
         public string Map { get; set; }
         public List<Npc> Npcs { get; set; }
         public List<MonsterBuilding> Monsters { get; set; }
