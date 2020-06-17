731b5040dacdc3361b386983f80424d865199ec0
Zach Campbell <zcam1995@gmail.com>
2020-06-17 19:57:33 +0100

    Adjusted map seed

--- DDCatalogue/Migrations/20200617183602_Changed Map seed to be accurate.Designer.cs
+++ DDCatalogue/Migrations/20200617183602_Changed Map seed to be accurate.Designer.cs
@@ -4,14 +4,16 @@ using DDCatalogue.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DDCatalogue.Migrations
 {
     [DbContext(typeof(DDContext))]
-    partial class DDContextModelSnapshot : ModelSnapshot
+    [Migration("20200617183602_Changed Map seed to be accurate")]
+    partial class ChangedMapseedtobeaccurate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DDCatalogue/Migrations/20200617183602_Changed Map seed to be accurate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DDCatalogue.Migrations
+{
+    public partial class ChangedMapseedtobeaccurate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Map",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LocaleId",
+                value: 6);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Map",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LocaleId",
+                value: 5);
+        }
+    }
+}
